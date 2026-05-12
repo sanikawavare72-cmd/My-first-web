8427b6d464014a6977951c12f5213802386b0158
sanikawavare72-cmd <sanikawavare72@gmail.com>
2026-05-12 16:09:25 +0530

    Add files via upload

---
+++ protfolio2.js
@@ -0,0 +1,7 @@
+/* =================== typing Animation ================================ */
+var typed = new Typed(".typing",{
+    strings:["Web Designer","Web Developer","Grapical Designer","YouTuber"],
+    typeSpeed:100,
+    backSpeed:100,
+    loop:true
+})
